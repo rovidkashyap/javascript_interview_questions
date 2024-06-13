0ec5ccd7401849adffe6f81d6871e3d3440a7a62
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-13 16:37:02 +0530

    Attach Event Listner

---
+++ Ques 37 - Add Event Listner/app.js
@@ -0,0 +1,8 @@
+// Find the button element by its ID
+const button = document.getElementById('myButton');
+
+// Add a click event listner to the button
+button.addEventListener('click', function(event) {
+    // This function will be executed when the button is clicked
+    console.log('Button Clicked!');
+})
